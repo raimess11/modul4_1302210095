92a1649c5ac83cd2d1ed3ec6d27e1ea1ac578ad9
raimess11 <rahma.sakti24@gmail.com>
2023-03-12 02:54:04 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302210095
+{
+    internal class Class1
+    {
+    }
+}
