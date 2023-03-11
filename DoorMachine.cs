4c379bfe29b4e21a656ca325fb4ad63019cbf92e
raimess11 <rahma.sakti24@gmail.com>
2023-03-12 03:36:31 +0700

    menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace tpmodul4_1302210095
+{
+    internal class DoorMachine
+    {
+        public DoorState currentState;
+        public enum DoorState { TERBUKA, TERKUNCI};
+        public enum Trigger { BUKAPINTU, KUNCIPINTU};
+        class Transition
+        {
+            public DoorState prevState;
+            public DoorState nextState;
+            public Trigger trigger;
+            public Transition(DoorState prevState, DoorState nextState, Trigger trigger)
+            {
+                this.prevState = prevState;
+                this.nextState = nextState;
+                this.trigger = trigger;
+            }
+        }
+        Transition[] transitions =
+        {
+            new Transition(DoorState.TERBUKA,DoorState.TERKUNCI,Trigger.KUNCIPINTU),
+            new Transition(DoorState.TERKUNCI,DoorState.TERKUNCI,Trigger.KUNCIPINTU),
+            new Transition(DoorState.TERKUNCI,DoorState.TERBUKA,Trigger.BUKAPINTU),
+            new Transition(DoorState.TERBUKA,DoorState.TERBUKA,Trigger.BUKAPINTU),
+        };
+        public DoorState getNextState(DoorState prevState, Trigger trigger)
+        {
+            DoorState nextState = prevState;
+            foreach(Transition transition in transitions){
+                if(transition.prevState == prevState && transition.trigger == trigger)
+                {
+                    nextState = transition.nextState;
+                }
+            }
+            return nextState;
+        }
+        public void activeTrigger(Trigger trigger)
+        {
+            DoorState nextState = getNextState(currentState, trigger);
+            currentState = nextState;
+
+            Console.WriteLine("PINTU " + ((currentState == DoorState.TERKUNCI) ? "TERKUNCI":"TIDAK TERKUNCI"));
+        }
+    }
+}
--- KodePos.cs
+++ KodePos.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace tpmodul4_1302210095
 {
-    internal class Class1
+    internal class KodePos
     {
+        public enum Kelurahan
+        {
+            Batununggal ,
+            Kujangsari ,
+            Mengger ,
+            Wates ,
+            Cijaura ,
+            Jatisari ,
+            Margasari ,
+            Sekejati ,
+            Kebonwaru ,
+            Maleer ,
+            Samoja ,
+        }
+        public int getKodePos(Kelurahan kelurahan)
+        {
+            int[] kodePos =
+            {
+                40266,
+                40287,
+                40267,
+                40256,
+                40287,
+                40286,
+                40286,
+                40286,
+                40272,
+                40274,
+                40273
+            };
+            return kodePos[(int)kelurahan];
+        }
     }
 }
